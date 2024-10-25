3f4cca3c668074b8bdf6f6beaedff344aa3ab2a4
meowmeow321-code <kontoszkola98765@gmail.com>
2024-10-25 11:23:19 +0200

    Update Avatar.jsx

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Avatar() {
     return (
-        <Image src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png" alt="Avatar" width={100} height={100} className="rounded-full"/>
+        <Image src="https://cdn.jsdelivr.net/gh/alohe/avatars/png/3d_5.png" alt="Avatar" width={100} height={100} className="rounded-full"/>
     )
 }
