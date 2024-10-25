b8208c8267282a717fbbd15e15ef72704b949fbe
meowmeow321-code <kontoszkola98765@gmail.com>
2024-10-25 11:16:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,6 @@ export const config = {
     description: 'solo developer by day, overthinker by night.',
     links: [
         {
-            slug: 'github',
-            type: 'url',
-            link: 'https://github.com/asrvd/'
-        },
-        {
-            slug: 'discord',
-            type: 'hover',
-            text: 'asheeshh#4872'
-        },
-        {
             slug: 'twitter',
             type: 'url',
             link: 'https://twitter.com/_asheeshh'
@@ -25,46 +15,6 @@ export const config = {
             slug: 'instagram',
             type: 'url',
             link: 'https://instagram.com/asrvd_'
-        },
-        {
-            slug: 'spotify',
-            type: 'url',
-            link: 'https://open.spotify.com/user/31ma7274fwzhcig5iibz25thypoi?si=b8e4d9aaa0e7425e'
-        },
-        {
-            slug: 'devdotto',
-            type: 'url',
-            link: 'https://dev.to/asheeshh'
-        },
-        {
-            slug: 'lastdotfm',
-            type: 'url',
-            link: 'https://last.fm/user/asheeshh'
-        },
-        {
-            slug: 'kitsu',
-            type: 'url',
-            link: 'https://kitsu.io/users/1258735'
-        },
-        {
-            slug: 'buymeacoffee',
-            type: 'url',
-            link: 'https://www.buymeacoffee.com/asheeshh'
-        },
-        {
-            slug: 'minutemailer',
-            type: 'url',
-            link: 'mailto:asheeshh@duck.com'
-        },
-        {
-            slug: 'npm',
-            type: 'url',
-            link: 'https://www.npmjs.com/~asheeshh'
-        },
-        {
-            slug: 'reddit',
-            type: 'url',
-            link: 'https://reddit.com/u/asheeeshh'
         }
     ]
 }
